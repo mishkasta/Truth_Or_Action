8f514d5c1bdae7968d43150e9a3ef09537b2ef41
Vadzka500 <Vad-grodno@mail.ru>
2023-12-15 07:27:23 +0300

    UI Design pack fragment Ui design select players

--- app/src/main/java/com/maxelfs/truthanddare/MainActivity.kt
+++ app/src/main/java/com/maxelfs/truthanddare/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.maxelfs.truthanddare
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.MotionEvent
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.MobileAds
 import com.maxelfs.truthanddare.interfaces.*
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Locale
 import javax.inject.Inject
+import android.util.DisplayMetrics
+
+
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -22,6 +29,16 @@ class MainActivity : AppCompatActivity() {
 
         MobileAds.initialize(this)
         activityProvider.initialize(this)
+
+        /*var currentLocale = getSharedPreferences("com.maxelfs.truthoraction.PREFERENCES", Context.MODE_PRIVATE).getString("PrefLanguage", "");
+        if (currentLocale == "") {
+            val configuration: Configuration = resources.configuration
+            val locale = Locale("en")
+            Locale.setDefault(locale)
+            configuration.setLocale(locale)
+            val dm: DisplayMetrics = resources.displayMetrics
+            resources.updateConfiguration(configuration, dm)
+        }*/
 
         tooltipService.initialize(applicationContext, findViewById(R.id.rootView))
     }
--- app/src/main/java/com/maxelfs/truthanddare/models/Locale.kt
+++ app/src/main/java/com/maxelfs/truthanddare/models/Locale.kt
@@ -3,7 +3,8 @@ package com.maxelfs.truthanddare.models
 enum class Locale(
     val id: Int,
     val code: String) {
-    RU(2, "ru"),
-    EN(1, "en")
+    EN(1, "en"),
+    RU(2, "ru")
+
 
 }
